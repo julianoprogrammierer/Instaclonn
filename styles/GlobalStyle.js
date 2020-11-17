a51d3a56aa91e73d76ce5e9559bea8ebfe7d0345
julianoprogrammierer <julianoprogrammierer@gmail.com>
2020-11-17 18:32:15 -0300

    gitclon 1.3

--- styles/GlobalStyle.js
+++ styles/GlobalStyle.js
@@ -1,5 +1,5 @@
 import { createGlobalStyle } from "styled-components";
-
+import px2vw from "../pages/_px2vw";
 export default createGlobalStyle`
 * {
     margin: 0;
@@ -23,4 +23,16 @@ body {
     
     
 }
+
+:root {
+      font-size: ${px2vw(24)};
+
+      @media (min-width: 768px) {
+        font-size: ${px2vw(18)};
+      }
+
+      @media (min-width: 1024px) {
+        font-size: ${px2vw(16)};
+      }
+    }
 `;
